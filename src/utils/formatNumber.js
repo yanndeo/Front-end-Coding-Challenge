9fd4abbdded40bc8a4d916b9625e0b9a85eb21a7
Yanndeo <yan2sambou@gmail.com>
2019-05-31 11:45:06 +0000

    Create formatNumber function to format stars number : like 18K ...

---
+++ src/utils/formatNumber.js
@@ -0,0 +1,22 @@
+
+export const formatNumber = (num ) =>{
+    
+    let stars;
+    console.log(num)
+
+    if (num.toString().length > 3) {
+
+        stars = num / 1000;
+
+        stars.toFixed()
+
+        return stars = `${stars.toFixed()}K`;
+
+        //console.log(stars)
+
+    } else {
+
+        return  stars = num;
+    }
+
+}
